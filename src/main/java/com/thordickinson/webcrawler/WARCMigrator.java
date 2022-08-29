beb1b738b93b610766575411485c592ddf0fa12b
Dickinson Arismendy <thordickinson@gmail.com>
2022-08-28 20:30:19 -0500

    Tests with ORC format

--- src/main/java/com/thordickinson/webcrawler/WARCMigrator.java
+++ src/main/java/com/thordickinson/webcrawler/WARCMigrator.java
@@ -15,18 +15,19 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.sql.Timestamp;
 import java.util.function.Consumer;
 
 public class WARCMigrator {
 
-    public void migrate(String path){
-        Path root = Path.of("./data/jobs/meli/executions/{}".formatted(path));
-        for (var file : root.toFile().listFiles()){
-            migrateFile(file);
+    public void migrate(String path) throws IOException {
+        Path root = Path.of("./data/jobs/meli/executions/%s/warc".formatted(path));
+        var rootDir = root.toFile();
+        for (var file : rootDir.listFiles()){
+            migrateFile(file.toPath());
         }
-
     }
 
 
@@ -34,14 +35,14 @@ public class WARCMigrator {
 
         var targetPath = file.getParent().resolve(file.getFileName() + ".orc");
         var target = new org.apache.hadoop.fs.Path(targetPath.toString());
-        var struct = "struct<timestamp:timestamp,url:string,content:string>";
+        var struct = "struct<timestamp:timestamp,url:string>";
         TypeDescription schema = TypeDescription.fromString(struct);
         VectorizedRowBatch batch = schema.createRowBatch();
         Configuration configuration = new Configuration();
 
         TimestampColumnVector timestampColumn = (TimestampColumnVector) batch.cols[0];
         BytesColumnVector urlColumn = (BytesColumnVector) batch.cols[1];
-        BytesColumnVector contentColumn = (BytesColumnVector) batch.cols[2];
+        //BytesColumnVector contentColumn = (BytesColumnVector) batch.cols[2];
 
         ByteBuffer buffer = ByteBuffer.allocate(100_000);
 
@@ -53,7 +54,9 @@ public class WARCMigrator {
                     int rowNum = batch.size++;
                     timestampColumn.set(rowNum, Timestamp.from(record.date()));
                     response.body().read(buffer);
-                    urlColumn.setVal(rowNum, buffer.array());
+                    var urlBytes = response.target().getBytes(StandardCharsets.UTF_8);
+                    urlColumn.setRef(rowNum, urlBytes, 0, urlBytes.length);
+                    //contentColumn.setVal(rowNum, buffer.array());
                     if(batch.size == batch.getMaxSize()){
                         writer.addRowBatch(batch);
                         batch.reset();
@@ -64,7 +67,7 @@ public class WARCMigrator {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new WARCMigrator().migrate("1661552637911");
     }
 }
