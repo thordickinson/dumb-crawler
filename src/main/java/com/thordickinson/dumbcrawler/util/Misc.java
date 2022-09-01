6761c0c7fd1526dad527d65b888c68b96569e398
Dickinson Arismendy <thordickinson@gmail.com>
2022-09-01 09:19:58 -0400

    feat: logging null host urls

--- src/main/java/com/thordickinson/dumbcrawler/util/Misc.java
+++ src/main/java/com/thordickinson/dumbcrawler/util/Misc.java
@@ -3,11 +3,19 @@ package com.thordickinson.dumbcrawler.util;
 import java.net.URI;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Misc {
+
+    private static final Logger logger = LoggerFactory.getLogger(Misc.class);
 
     public static Optional<URI> parseURI(String uri) {
         try {
             var parsed = URI.create(uri);
+            if (parsed.getHost() == null) {
+                logger.warn("URL does not have host info {}", uri);
+            }
             return Optional.ofNullable(parsed);
         } catch (IllegalArgumentException ex) {
             return Optional.empty();
